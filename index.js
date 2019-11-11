ce4a3b870cb71fd836e49a9413d347dcf972e160
Jovanoska <marija.jovanoska.maca@gmail.com>
2019-11-11 21:56:08 +0100

    cas 3 mongoose dodavanje

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://dev:****!cluster0-hldqz.mongodb.net:27017/videoteka', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://dev:DEV123!@cluster0-hldqz.mongodb.net/school?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(res=> {
     console.log(res);
     
@@ -9,24 +9,55 @@ mongoose.connect('mongodb://dev:****!cluster0-hldqz.mongodb.net:27017/videoteka'
     console.log(err);
 });
 
-const Filmovi = mongoose.model(
-    'Filmovi',
-     { 
-         ime: String,
-         godina: Date,
-         zanr: [String],
-         rezija: String,
-         oskar: Boolean,
-         akteri : [String]
-        
-        });
-
-    Filmovi.find((err, data) => {
-        if(err){
-            console.log('ERROR!')
+const Student = mongoose.model(
+    'student', new mongoose.Schema(
+        {
+            first_name : String,
+            last_name : String, 
+            average_grade : Number,
+            courses : [String],
+            email : String,
+            birthday : Date
         }
-        console.log(data)
-    })
+    )
+)
+
+var s = new Student({
+            first_name : "Janko",
+            last_name : "Jankovski", 
+            average_grade : 5,
+            courses : ["math", "science", "economy"],
+            email : "janko@gmail.com",
+            birthday : new Date ('1994-11-11T19:30:00Z')
+})
+
+var a = new Student({
+    first_name : "Stanko",
+    last_name : "Stankovski", 
+    average_grade : 4.5,
+    courses : ["geography", "science", "sports"],
+    email : "stanko@gmail.com",
+    birthday : new Date ('1992-09-08T19:30:00Z')
+})
+var b = new Student({
+    first_name : "Dime",
+    last_name : "Pavlov", 
+    average_grade : 5,
+    courses : ["math", "science", "architecture"],
+    email : "stanko@gmail.com",
+    birthday : new Date ('1994-10-10T19:30:00Z')
+})
 
 
-console.log('end!');
+var k = [a,s,b]
+
+
+k.forEach((g, i) => {
+    g.save(err => {
+        if(err){
+            console.log(err)
+        }
+        console.log('save successfully')
+    });
+});
+
