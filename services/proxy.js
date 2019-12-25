471f96a931fa3cb612c397b0524185aecf02c2dc
Jovanoska <marija.jovanoska.maca@gmail.com>
2019-12-25 21:31:00 +0100

    trying to make it work

--- services/proxy.js
+++ services/proxy.js
@@ -5,15 +5,22 @@ var api = express();
 var apiProxy= proxy.createProxyServer();
 
 api.all('/api/v1/auth/*', (req, res) => {
-    apiProxy.web(req, res, {target: 'http://localhost:8001'});
+    apiProxy.web(req, res, {target: 'http://localhost:8081'});
 });
 
 api.all('/api/v1/files/*', (req, res) => {
-    apiProxy.web(req, res, {target: 'http://localhost:8002'});
+    apiProxy.web(req, res, {target: 'http://localhost:8082'});
 });
 
 api.all('/api/v1/filmovi/*', (req, res) => {
-    apiProxy.web(req, res, {target: 'http://localhost:8000'});
+    apiProxy.web(req, res, {target: 'http://localhost:8080'});
 });
 
-api.listen(5000);
+api.listen(5000, err => {
+    if(err){
+        console.log('could not start server');
+        console.log(err);
+        return;
+    }
+    console.log('server started sucessfully on port 8080');
+});
