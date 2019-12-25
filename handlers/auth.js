6d2c77ecd4b695e73aaddeb9d46217decdc90673
Jovanoska <marija.jovanoska.maca@gmail.com>
2019-12-25 21:52:36 +0100

    trying to make it work

--- handlers/auth.js
+++ handlers/auth.js
@@ -116,5 +116,6 @@ module.exports = {
     renew,
     resetLink,
     resetPassword,
-    changePassword
+    changePassword,
+    confirm
 }
--- services/proxy.js
+++ services/proxy.js
@@ -16,11 +16,15 @@ api.all('/api/v1/filmovi/*', (req, res) => {
     apiProxy.web(req, res, {target: 'http://localhost:8080'});
 });
 
-api.listen(5000, err => {
+api.all('/*', (req, res) =>{
+    res.status(404).send('Not Found!');
+});
+
+api.listen(process.env.PORT, err => {
     if(err){
         console.log('could not start server');
         console.log(err);
         return;
     }
-    console.log('server started sucessfully on port 8080');
+    console.log('server started sucessfully on port heroku');
 });
